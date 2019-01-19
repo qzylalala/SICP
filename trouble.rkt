31c2ac4cbac78417702577ff8b20fab224c02fb7
qzylalala <304228244@qq.com>
2019-01-19 23:22:13 +0800

    there are some trouble

---
+++ trouble.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname trouble) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (cubes x)
+  (* x x x))
+(cubes 10)
