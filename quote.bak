a0a80a217f3605f6cb06950a48b46539bdfe2ac2
qzylalala <43845521+qzylalala@users.noreply.github.com>
2020-08-13 15:59:01 +0800

    quote

---
+++ quote.bak
@@ -0,0 +1,8 @@
+#lang sicp
+(define a 1)
+(define b 2)
+(display (list a b))
+newline
+(display (list 'a b))
+newline
+(display (list a 'b))
--- quote.rkt
+++ quote.rkt
@@ -2,7 +2,10 @@
 (define a 1)
 (define b 2)
 (display (list a b))
-newline
 (display (list 'a b))
-newline
 (display (list a 'b))
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
