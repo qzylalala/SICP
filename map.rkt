b5b381038302fe91973bb875e743163eb8851381
qzylalala <43845521+qzylalala@users.noreply.github.com>
2020-08-13 14:32:56 +0800

    map, pair?

---
+++ map.rkt
@@ -0,0 +1,7 @@
+#lang sicp
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree sub-tree factor)
+             (* sub-tree factor)))
+       tree))
